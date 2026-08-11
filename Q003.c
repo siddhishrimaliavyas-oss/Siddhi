76811ffa06f964c7ccc8266c1a4b21a332f33008
siddhishrimaliavyas-oss <siddhishrimali.avyas@gmail.com>
2026-08-11 22:39:57 +0530

    Update Q003.c

--- Q003.c
+++ Q003.c
@@ -7,9 +7,9 @@ int main() {
     printf ("Enter the breadth of rectangle:");
     scanf ("%d" , & b);
     peri = 2*l+2*b;
-    printf ("The perimeter of the rectangle is %d \n" , & peri);
+    printf ("The perimeter of the rectangle is %d \n" ,  peri);
     area = l*b;
-    printf ("The area of given rectangle is %d \n" , & area);
+    printf ("The area of given rectangle is %d \n" ,  area);
 
     return 0;
 
