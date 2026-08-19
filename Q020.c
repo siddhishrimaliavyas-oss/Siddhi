a954e3a31c3a17da29b8c987b5001745d1901516
siddhishrimaliavyas-oss <siddhishrimali.avyas@gmail.com>
2026-08-19 22:12:14 +0530

    Add files via upload

---
+++ Q020.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    printf ("Enter a number:");
+    scanf ("%d" , & a);
+    switch (a)
+    {
+        case 1 :
+            printf ("MONDAY");
+        break;
+        case 2 :
+            printf ("TUESDAY");
+        break;
+        case 3 :
+            printf ("WEDNESDAY");
+        break;
+        case 4 :
+            printf ("THURSDAY");
+        break;
+        case 5 :
+            printf ("FRIDAY");
+        break;
+        case 6 :
+            printf ("SATURDAY");
+        break;
+        case 7 :
+            printf ("SUNDAY");
+        break;
+        default:
+        printf ("WRONG INPUT");
+    }
+
+    return 0;
+
+}
