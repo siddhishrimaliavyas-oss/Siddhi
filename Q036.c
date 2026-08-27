d62d2606a1a7e4c4e512e546559bd16bb663d103
siddhishrimaliavyas-oss <siddhishrimali.avyas@gmail.com>
2026-08-27 17:53:58 +0530

    Add files via upload

---
+++ Q036.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num1 , num2 , a , b , temp;
+    printf ("Enter two numbers:");
+    scanf ("%d %d", & num1 , & num2);
+    a = num1;
+    b = num2;
+    while ( b != 0){
+        temp = b;
+        b = a % b;
+        a = temp;
+    }
+    printf ("The HCF of %d and %d is %d \n" , num1 ,  num2 , a);
+
+    return 0;
+
+}
