e79d9124a3e4a7644eb7b7753be30e82ed360042
siddhishrimaliavyas-oss <siddhishrimali.avyas@gmail.com>
2026-09-14 19:59:24 +0530

    Add files via upload

---
+++ Q072.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols;
+    scanf ("%d %d", &rows, &cols);
+    int matrix[rows][cols];
+    int sum = 0;
+    for (int i = 0; i< rows; i++){
+        for (int j = 0; j < cols; j++){
+            scanf ("%d", &matrix[i][j]);
+            sum += matrix[i][j];
+        }
+    }
+    printf("%d\n" , sum);
+
+    return 0;
+
+}
