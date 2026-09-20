3d18ec06e571791c156e95baa12e1be08d22c47c
siddhishrimaliavyas-oss <siddhishrimali.avyas@gmail.com>
2026-09-20 21:59:42 +0530

    Add files via upload

---
+++ Q083.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+
+    fgets (str, sizeof(str) , stdin);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = tolower(str[i]);
+
+        if (ch >= 'a' && ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+    printf ("Vowels = %d , Consonants=%d\n" , vowels , consonants);
+
+    return 0;
+
+}
